8ca5bb79776a6d2ef5148d21eb7ed36ac7b69b8a
Garrett Griffin <grgisme@gmail.com>
2016-04-26 17:43:04 -0400

    Testing Deployments and Resource Plan Syncing

--- SURFBrowser.js
+++ SURFBrowser.js
@@ -40,49 +40,34 @@ SURFBrowser.prototype.setPassword = function (password) {
     this.password = password;
 };
 
-SURFBrowser.prototype.login = function() {
-
-    //if(this.username == undefined || this.username == "" || this.password == undefined || this.password == "")
-    //    return false;
-
-    return new Promise( function( resolve, reject ){
-        return SURFBrowser().browser
-            .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
-            //.on('resourceError', function(resourceError) { console.log(resourceError.errorString); console.log(StringresourceError.url); })
-            //.on('urlChanged', function(targetUrl) { console.log("Navigating to: "+targetUrl)})
-            .on('timeout', function(msg){ console.log("Timeout: "+msg); })
-            .open('https://surf.service-now.com/')
-            .waitForSelector('#username')
-            .log("At Login Page")
-            //.screenshot("loginpage.png")
-            .text('h1.headingText')
-            .type('#username', SURFBrowser().username)
-            //.keyboardEvent("keypress", 16777217)
-            .type('#password', SURFBrowser().password)
-            .click("#submitButton")
-            .waitForSelector('#mainBannerImage16')
-            .wait(5000)
-            .log("At SURF Home Page")
-            //.screenshot("homepage.png")
-            .then(this.markLoggedIn)
-            .then( resolve );
-    });
-};
-
 SURFBrowser.prototype.grabTimeCards = function(callBackFunction) {
     this.browser
         .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
-        .open('https://google.com/')
-        .then(this.login)
-        .open("https://surf.service-now.com/time_card_list.do?JSONv2")
-        .waitForNextPage()
+        .log("About to grab Resource Plans -- logging in")
+        //.on('resourceError', function(resourceError) { console.log(resourceError.errorString); console.log(StringresourceError.url); })
+        //.on('urlChanged', function(targetUrl) { console.log("Navigating to: "+targetUrl)})
+        //.on('timeout', function(msg){ console.log("Timeout: "+msg); })
+        .open('https://surf.service-now.com/')
+        .waitForSelector('#username')
+        .log("At Login Page")
+        //.screenshot("loginpage.png")
+        .text('h1.headingText')
+        .type('#username', SURFBrowser().username)
+        //.keyboardEvent("keypress", 16777217)
+        .type('#password', SURFBrowser().password)
+        .click("#submitButton")
+        .waitForSelector('#mainBannerImage16')
         .wait(5000)
-        .text()
+        .log("At SURF Home Page")
+        .log("Logged In, now going to grab the time card list")
+        .open("https://surf.service-now.com/time_card_list.do?JSONv2")
+        .wait(5000)
+        .text('pre')
         .then(callBackFunction);
 };
 
 SURFBrowser.prototype.grabDeployments = function(callBackFunction) {
-    this.browser
+    new Horseman()
         .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
         .log("About to grab Deployments -- logging in")
         //.on('resourceError', function(resourceError) { console.log(resourceError.errorString); console.log(StringresourceError.url); })
@@ -108,13 +93,13 @@ SURFBrowser.prototype.grabDeployments = function(callBackFunction) {
         .then(callBackFunction);
 };
 
-SURFBrowser.prototype.grabDeploymentsCSV = function(callBackFunction) {
-    this.browser
+SURFBrowser.prototype.grabResourcePlans = function(callBackFunction) {
+    new Horseman()
         .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
-        .log("About to grab Deployments CSV -- logging in")
+        .log("About to grab Resource Plans -- logging in")
         //.on('resourceError', function(resourceError) { console.log(resourceError.errorString); console.log(StringresourceError.url); })
         //.on('urlChanged', function(targetUrl) { console.log("Navigating to: "+targetUrl)})
-        .on('timeout', function(msg){ console.log("Timeout: "+msg); })
+        //.on('timeout', function(msg){ console.log("Timeout: "+msg); })
         .open('https://surf.service-now.com/')
         .waitForSelector('#username')
         .log("At Login Page")
@@ -127,35 +112,10 @@ SURFBrowser.prototype.grabDeploymentsCSV = function(callBackFunction) {
         .waitForSelector('#mainBannerImage16')
         .wait(5000)
         .log("At SURF Home Page")
-        .log("Logged In, now going to grab the deployment list")
-        .open("https://surf.service-now.com/u_deployment_list.do?CSV")
-        .waitForNextPage()
-        .text()
-        .then(callBackFunction);
-};
-
-SURFBrowser.prototype.grabDeploymentsEXCEL = function(callBackFunction) {
-    this.browser
-        .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
-        .log("About to grab Deployments EXCEL -- logging in if needed")
-        .open('https://google.com/')
-        .then(this.login)
-        .log("Logged In, now going to grab the deployment list")
-        .open("https://surf.service-now.com/u_deployment_list.do?EXCEL")
-        .waitForNextPage()
-        .text()
-        .then(callBackFunction);
-};
-
-SURFBrowser.prototype.grabResourcePlans = function(callBackFunction) {
-    this.browser
-        .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
-        .open('https://google.com/')
-        .then(this.login)
+        .log("Logged In, now going to grab the resource plan list")
         .open("https://surf.service-now.com/u_billing_rates_list.do?JSONv2")
-        .waitForNextPage()
         .wait(5000)
-        .text()
+        .text('pre')
         .then(callBackFunction);
 };
 
--- SURFConnector.js
+++ SURFConnector.js
@@ -12,6 +12,7 @@ var deasync = require('deasync');
 //const Browser = require('zombie');
 var done = false;
 var data;
+var numDone = 0;
 
 
 var SURFBrowser = require("./SURFBrowser");
@@ -19,64 +20,33 @@ var SURFBrowser = require("./SURFBrowser");
 var surf = new SURFBrowser();
 surf.setCredentials(config.surfUsername, config.surfPassword);
 surf.grabDeployments(processDeployments);
-//surf.grabDeploymentsCSV(processDeploymentsCSV);
+surf.grabResourcePlans(processResourcePlans);
+surf.grabTimeCards(processTimeCards);
+deasync.loopWhile(function(){if(numDone < 3) return false; else return true;});
+surf.close();
 
 
 
 
 function processDeployments(text) {
-    console.log("Made it to the callback function.");
+    console.log("Processing Deployments");
     postJSONToImportSet("u_import_deployment",text);
-    surf.close();
-}
-function processDeploymentsCSV(text) {
-    console.log("Made it to the callback function.");
-    postCSVToImportSet("u_import_deployment",text);
-    surf.close();
 }
 
-function normalizeJSON(theArr) {
-    var keys = [];
-    for(var i=0; i<theArr.length; i++) {
-        var obj = theArr[i];
-        for(var x in obj) {
-            if(keys.indexOf(x) < 0)
-                keys.push(x);
-        }
-    }
-    for(var j=0; j<theArr.length; j++) {
-        var theObj = theArr[j];
-        for(var k in keys) {
-            if(typeof theObj[k] == "undefined") {
-                theObj[k] = "";
-            }
-        }
-    }
-    return theArr;
+function processResourcePlans(text) {
+    console.log("Processing Resource Plans");
+    postJSONToImportSet("u_import_resource_plans", text);
+}
+
+function processTimeCards(text) {
+    console.log("Processing Time Cards");
+    console.log("Doing nothing to process Time Cards for now.");
+    //postJSONToImportSet("u_import_resource_plans", text);
+    done++;
 }
 
 function textToFile(text, fileName) {
     return fs.writeFileSync(fileName, text);
-}
-
-function postCSVToImportSet(importSetName, csv) {
-    var tmpobj = tmp.fileSync();
-    var fileName = tmpobj.name;
-    textToFile(csv, fileName);
-    fs.writeFileSync("deployments.csv", csv);
-
-    fs.stat(fileName, function(err, stats) {
-        rest.post(config.cernaHubURL+"/sys_import.do?sysparm_import_set_tablename="+importSetName+"&sysparm_transform_after_load=true", {
-            multipart: true,
-            username: config.cernaHubUsername,
-            password: config.cernaHubPassword,
-            data: {
-                'uploadfile': rest.file(fileName, null, stats.size, null, 'text/csv')
-            }
-        }).on("complete", function(data) {
-            console.log(data);
-        });
-    });
 }
 
 function json2csv(theArray) {
@@ -107,12 +77,7 @@ function postJSONToImportSet(importSetName, text) {
             }
         }).on("complete", function(data) {
             fs.unlinkSync(fileName);
+            numDone++;
         });
     });
-    /*
-     var timecards = JSON.parse(text);
-     timecards = timecards.records;
-     console.log("Number of TimeCards Grabbed: "+timecards.length);
-     console.log(timecards[0]);
-     */
 }
