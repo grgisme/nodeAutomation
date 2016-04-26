54060a41f1da326898415e69284eb274dfa827d3
Garrett Griffin <grgisme@gmail.com>
2016-04-26 11:17:08 -0400

    Debugging

--- SURFBrowser.js
+++ SURFBrowser.js
@@ -47,6 +47,7 @@ SURFBrowser.prototype.login = function() {
     return new Promise( function( resolve, reject ){
         return SURFBrowser().browser
             .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
+            .on('error', function(msg, trace){ console.log(msg); console.trace })
             .open('https://surf.service-now.com/')
             .waitForSelector('#username')
             .log("At Login Page")
